6a5fceb27ab967033c6dc4501ecba01f730ca756
Aidan Timson <aidan@timmo.dev>
2025-10-31 00:05:37 +0000

    Use relative path

--- log-viewer/frontend/vite.config.js
+++ log-viewer/frontend/vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   root: './',
   publicDir: 'public',
+  base: './', // Use relative paths for ingress compatibility
   build: {
     outDir: process.env.BUILD_OUT_DIR || '../rootfs/opt/logviewer/public',
     emptyOutDir: true,
